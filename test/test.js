f4fd0657a8889db6c87505efb734548887fd146d
Eirik S. Morland <eirik@morland.no>
2014-10-09 19:06:14 +0200

    Add coverage.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,12 @@
 require('should');
 var a = require('../index')
 describe('Beer module', function() {
-  it('Should return the expected value', function() {
+  it('Should return the expected value', function(done) {
     a(1.050, 1.010).toFixed(2).should.equal('5.24');
+    done();
+  });
+  it('Should complain', function(done) {
+    a.should.throw();
+    done();
   });
 });
